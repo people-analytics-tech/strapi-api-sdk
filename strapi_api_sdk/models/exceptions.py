0800e874c884a2316a1ff36670c9fc0b94134f3d
rapha-pereira <raphaelpgomes1@gmail.com>
2023-08-08 13:14:22 -0300

    feat: base exceptions model

---
+++ strapi_api_sdk/models/exceptions.py
@@ -0,0 +1,13 @@
+class StrapiExceptions(Exception):
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+
+
+class AuthenticationError(StrapiExceptions):
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+
+
+class ClientError(StrapiExceptions):
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
